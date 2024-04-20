0dadf2f0ece24691853d9121ede37f6626a6d5be
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-04-20 07:01:40 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+
+
+
+# DL model config
+DETECTION_MODEL_DIR = 'D:/Data/models'
+
+DETECTION_MODEL_LIST = [
+    'yolov8n.pt',
+    'yolov8s.pt',
+    'yolov8m.pt',
+    'yolov8l.pt',
+    'yolov8x.pt',
+    'yolov9c.pt',
+    'yolov9e.pt'
+]
