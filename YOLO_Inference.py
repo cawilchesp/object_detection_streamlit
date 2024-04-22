f335bbb15d5dbb4d2a70a99c772baf56317b9ade
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-04-22 16:05:18 -0500

    Update YOLO_Inference.py

--- YOLO_Inference.py
+++ YOLO_Inference.py
@@ -1,47 +1,42 @@
-from pathlib import Path
-from PIL import Image
 import streamlit as st
 
-import config
-from tools.utilities import load_model, infer_uploaded_image, infer_uploaded_video, infer_uploaded_webcam
 
-# setting page layout
-st.set_page_config(
-    page_title="Interactive Interface for YOLOv8",
-    page_icon="🤖",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+def main():
+    # setting page layout
+    st.set_page_config(
+        page_title="Interactive Interface for YOLOv8",
+        page_icon="🤖",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
 
-# main page heading
-st.title("YOLOv8 Inference")
+    # main page heading
+    st.title("YOLOv8 Inference")
 
-st.markdown(
+    st.markdown(
+        """
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+        **👈 Select a demo from the sidebar** to see some examples
+        of what Streamlit can do!
+        ### Want to learn more?
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+            forums](https://discuss.streamlit.io)
+        ### See more complex demos
+        - Use a neural net to [analyze the Udacity Self-driving Car Image
+            Dataset](https://github.com/streamlit/demo-self-driving)
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+    )
+
+    # source_img = None
+    # if source_selectbox == config.SOURCES_LIST[0]: # Image
+    #     infer_uploaded_image(confidence, model)
+    # elif source_selectbox == config.SOURCES_LIST[2]: # Webcam
+    #     infer_uploaded_webcam(confidence, model)
 
 
-
-# source_img = None
-# if source_selectbox == config.SOURCES_LIST[0]: # Image
-#     infer_uploaded_image(confidence, model)
-# elif source_selectbox == config.SOURCES_LIST[1]: # Video
-#     infer_uploaded_video(confidence, model)
-# elif source_selectbox == config.SOURCES_LIST[2]: # Webcam
-#     infer_uploaded_webcam(confidence, model)
-# else:
-#     st.error("Currently only 'Image' and 'Video' source are implemented")
+if __name__ == '__main__':
+    main()
