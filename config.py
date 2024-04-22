fbf9f12356cebc9c60f715d02f7a8b6a6ed575ad
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-04-22 10:51:22 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,39 @@
 from ultralytics import YOLO
 import streamlit as st
+from pathlib import Path
+import sys
+
+
+ROOT = Path('D:/Data')
+
+# Source
+SOURCES_LIST = ["Image", "Video", "Webcam"]
 
 # DL model config
-DETECTION_MODEL_DIR = 'D:/Data/models'
+DETECTION_MODEL_DIR = ROOT / 'models' / 'yolov8'
+YOLOv8n = DETECTION_MODEL_DIR / "yolov8n.pt"
+YOLOv8s = DETECTION_MODEL_DIR / "yolov8s.pt"
+YOLOv8m = DETECTION_MODEL_DIR / "yolov8m.pt"
+YOLOv8l = DETECTION_MODEL_DIR / "yolov8l.pt"
+YOLOv8x = DETECTION_MODEL_DIR / "yolov8x.pt"
 
 DETECTION_MODEL_LIST = [
-    'yolov8n.pt',
-    'yolov8s.pt',
-    'yolov8m.pt',
-    'yolov8l.pt',
-    'yolov8x.pt',
-    'yolov9c.pt',
-    'yolov9e.pt'
-]
+    "yolov8n.pt",
+    "yolov8s.pt",
+    "yolov8m.pt",
+    "yolov8l.pt",
+    "yolov8x.pt"]
 
 @st.cache_resource
-def load_model(model_path: str):
+def load_model(model_path):
+    """
+    Loads a YOLO object detection model from the specified model_path.
+
+    Parameters:
+        model_path (str): The path to the YOLO model file.
+
+    Returns:
+        A YOLO object detection model.
+    """
     model = YOLO(model_path)
     return model
